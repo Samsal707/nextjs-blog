fb021d9105a837aeb7478e87ff572e5d0d73c028
Sam Salani <samsal707@gmail.com>
2023-01-08 11:46:18 +1100

    first nexjs commit

---
+++ pages/authors/Me.js
@@ -0,0 +1,10 @@
+export default function MeAuthor() {
+  return (
+    <>
+        <h1>Authored by Me</h1>
+        <h2>
+         <Link href="/">Back to home</Link>
+        </h2>
+    </>
+  );
+}
---
+++ pages/posts/post-response.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function PostResponse() {
+  return(
+  <>
+        <h1>Authored by Me</h1>
+        <h2>
+         <Link href="/">Back to home</Link>
+        </h2>
+    </>
+    )
+}
